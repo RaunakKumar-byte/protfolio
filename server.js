2822656e4eeba8fb3b8002323530a2776a406bde
Raunak kumar <singhraunakkumar524@gmail.com>
2025-07-10 16:03:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,10 +29,7 @@ app.use((req, res, next) => {
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI , {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("MongoDB connection error:", err))
 
